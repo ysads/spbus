eeebcbb7b94c83ca0e87ea0fa5b76c6f4a6ac0c7
Ygor Sad <ygor.sad@revelo.com.br>
2019-10-23 20:41:27 -0300

    [test] substitute unmocked test for spreadsheet ns

--- test/spbus/adapters/spreadsheet_test.clj
+++ test/spbus/adapters/spreadsheet_test.clj
@@ -4,16 +4,16 @@
             [spbus.adapters.spreadsheet :as spreadsheet]
             [spbus.support.test-tools :as tools]))
 
-(def sheet-local (spreadsheet/load-sheet "./test/fixtures/sptrans/type_a.xls"))
-(def sheet-remote (spreadsheet/load-sheet "https://www.prefeitura.sp.gov.br/cidade/secretarias/upload/09012019_Dados%20para%20Transpar%C3%AAncia.xls"))
+(def sheet-1 (spreadsheet/load-sheet "./test/fixtures/sptrans/type_a.xls"))
+(def sheet-2 (spreadsheet/load-sheet "./test/fixtures/sptrans/type_b.xls"))
 
 (fact "spreadsheet/load-sheet opens a workbook and return its first sheet"
-  (class sheet-local) => org.apache.poi.hssf.usermodel.HSSFSheet
-  (class sheet-remote) => org.apache.poi.hssf.usermodel.HSSFSheet)
+  (class sheet-1) => org.apache.poi.hssf.usermodel.HSSFSheet
+  (class sheet-2) => org.apache.poi.hssf.usermodel.HSSFSheet)
 
 (fact "spreadsheet/rows converts sheet data into seq"
-  (seq? (spreadsheet/rows sheet-local)) => true
-  (seq? (spreadsheet/rows sheet-remote)) => true)
+  (seq? (spreadsheet/rows sheet-1)) => true
+  (seq? (spreadsheet/rows sheet-2)) => true)
 
 (defn sample-cell
   [sheet cell-number]
@@ -23,8 +23,8 @@
       (seq)
       (nth cell-number)))
 
-(def cell-4 (sample-cell sheet-local 4))
-(def cell-5 (sample-cell sheet-local 5))
+(def cell-4 (sample-cell sheet-1 4))
+(def cell-5 (sample-cell sheet-1 5))
 
 (fact "spreadsheet/cell-value extracts the value of a given cell"
   (spreadsheet/cell-value cell-4) => "N14311 - METRO BARRA FUNDA/MORRO GRANDE"
