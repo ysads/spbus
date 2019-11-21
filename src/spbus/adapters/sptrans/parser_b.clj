b15c3f9f91614e6e7e2db68db08c08eedb02ed6e
Ygor Sad <ygor.sad@revelo.com.br>
2019-11-21 18:14:37 -0300

    [parser-b] adapt pre-boarding? logic from parser-a ns

--- src/spbus/adapters/sptrans/parser_b.clj
+++ src/spbus/adapters/sptrans/parser_b.clj
@@ -4,6 +4,10 @@
             [spbus.adapters.spreadsheet :as spreadsheet])
   (:gen-class))
 
+(def terminus-first-char-index 9)
+(def terminus-inter-separator #"(-)|(/)")
+(def terminus-preboarding-tokens ["TSATER" "EXP TIRADENTES" "5105"])
+
 (defn ^:private line-code
   "Returns the line main code. Examples:
    - 803210 => 8032
@@ -34,20 +38,32 @@
     (str (line-code raw-id) "-" (branch-code raw-id))))
 
 (defn ^:private line-terminus
+  "The full line terminus, potentially including main and auxiliar
+  terminuses as long as some pre-boarding keywords."
   [row]
   (let [content (spreadsheet/cell-value row 4)]
-    (-> (str/split content #" - ")
-        (last))))
+    (str/upper-case (subs content terminus-first-char-index))))
+
+(defn ^:private terminus-include-pre-boarding-token?
+  "Returns true if line terminus contains any of the tokens
+  which indicates a pre-boarding."
+  [row]
+  (let [terminus (line-terminus row)]
+    (reduce (fn [result token]
+              (or result (str/includes? terminus token)))
+            false
+            terminus-preboarding-tokens)))
+
 
 (defn main-terminus
   [row]
   (let [terminus (line-terminus row)]
-    (first (str/split terminus #"/"))))
+    (str/trim (first (str/split terminus terminus-inter-separator)))))
 
 (defn auxiliar-terminus
   [row]
   (let [terminus (line-terminus row)]
-      (last (str/split terminus #"/"))))
+				(str/trim (last (str/split terminus terminus-inter-separator)))))
 
 (defn company
   [row]
@@ -117,28 +133,53 @@
   [row]
   (= 19 (count row)))
 
+(defn pre-boarding?
+  [row]
+  (let [id (line-id row)]
+    (or (some? (re-matches #"^([0-9]+PR).*" id))
+        (terminus-include-pre-boarding-token? row))))
+
+(defn basic-info
+  [row]
+  (let [id (line-id row)]
+    {:company (company row)
+     :line-id id
+     :line-code (line-code id)
+     :branch-code (branch-code id)}))
+
+(defn with-pre-boarding
+  [data row]
+  (merge data {:pre-boarding (pre-boarding? row)}))
+
+(defn with-terminus
+  [data row]
+  (merge data (if (pre-boarding? row)
+                  {:terminus (main-terminus row)}
+                  {:main-terminus (main-terminus row)
+                   :auxiliar-terminus (auxiliar-terminus row)})))
+
+(defn with-pax-totals
+  [data row]
+  (merge data {:paying-pax {:cash (paying-cash-pax row)
+										                  :normal (paying-normal-pax row)
+										                  :work-card (paying-work-card-pax row)
+										                  :student (paying-student-pax row)
+										                  :rail-bus-connections (paying-rail-bus-conn-pax row)
+										                  :month-pass-normal (paying-month-pass-normal-pax row)
+										                  :month-pass-work (paying-month-pass-work-pax row)
+										                  :month-pass-student (paying-month-pass-student-pax row)
+										                  :month-pass-rail-bus-connections (paying-month-pass-rail-bus-conn-pax row)
+										                  :total (paying-pax row)}
+										     :free-pax {:bus-bus-connections (free-bus-bus-conn-pax row)
+										                :normal (free-normal-pax row)
+										                :student (free-student-pax row)
+										                :total (free-pax row)}
+										     :total-pax (total-pax row)}))
+
 (defn parse
   "Returns a map containing all the line information"
   [row]
-  (let [id (line-id row)]
-    {:line-id id
-     :line-code (line-code id)
-     :branch-code (branch-code id)
-     :main-terminus (main-terminus row)
-     :auxiliar-terminus (auxiliar-terminus row)
-     :company (company row)
-     :paying-pax {:cash (paying-cash-pax row)
-                  :normal (paying-normal-pax row)
-                  :work-card (paying-work-card-pax row)
-                  :student (paying-student-pax row)
-                  :rail-bus-connections (paying-rail-bus-conn-pax row)
-                  :month-pass-normal (paying-month-pass-normal-pax row)
-                  :month-pass-work (paying-month-pass-work-pax row)
-                  :month-pass-student (paying-month-pass-student-pax row)
-                  :month-pass-rail-bus-connections (paying-month-pass-rail-bus-conn-pax row)
-                  :total (paying-pax row)}
-     :free-pax {:bus-bus-connections (free-bus-bus-conn-pax row)
-                :normal (free-normal-pax row)
-                :student (free-student-pax row)
-                :total (free-pax row)}
-     :total-pax (total-pax row)}))
+  (-> (basic-info row)
+      (with-pre-boarding row)
+      (with-terminus row)
+      (with-pax-totals row)))
--- test/spbus/adapters/sptrans/parser_b_test.clj
+++ test/spbus/adapters/sptrans/parser_b_test.clj
@@ -4,59 +4,78 @@
             [spbus.adapters.sptrans.parser-b :as parser-b]
             [spbus.support.test-tools :as tools]))
 
-(defn sample-row
-  [fixture-file]
-  (->> (spreadsheet/load-sheet fixture-file)
-       (spreadsheet/rows)
-       (drop 3)
-       (first)
-       (seq)))
+(defn nth-row-in-sheet
+  [sheet row-number]
+  (-> (spreadsheet/nth-row sheet row-number :header-size 3)
+      (seq)))
 
-(def row-type-a (sample-row "./test/fixtures/sptrans/type_a.xls"))
-(def row-type-b (sample-row "./test/fixtures/sptrans/type_b.xls"))
+(def row-type-a (-> (spreadsheet/load-sheet "./test/fixtures/sptrans/type_a.xls")
+                    (nth-row-in-sheet 0)))
+
+(def type-a-sheet (spreadsheet/load-sheet "./test/fixtures/sptrans/type_b.xls"))
+(def row-normal (nth-row-in-sheet type-a-sheet 0))
+(def row-pre-boarding (nth-row-in-sheet type-a-sheet 1))
+(def row-bad-format (nth-row-in-sheet type-a-sheet 2))
+(def row-exp-tiradentes (nth-row-in-sheet type-a-sheet 3))
 
 (fact "parser-b/parseable? returns true only for a valid type a row"
   (parser-b/parseable? row-type-a) => false
-  (parser-b/parseable? row-type-b) => true)
+  (parser-b/parseable? row-pre-boarding) => true
+  (parser-b/parseable? row-exp-tiradentes) => true
+  (parser-b/parseable? row-normal) => true)
 
 (fact "parser-b/line-id parses line ID"
-  (parser-b/line-id row-type-b) => "N10511")
+  (parser-b/line-id row-bad-format) => "536210"
+  (parser-b/line-id row-normal) => "N10511")
 
 (fact "parser-b/pretty-line-id formats line ID"
-  (parser-b/pretty-line-id row-type-b) => "N105-11")
+  (parser-b/pretty-line-id row-normal) => "N105-11")
 
 (fact "parser-b/main-terminus parses main terminus"
-  (parser-b/main-terminus row-type-b) => "TERM CACHOEIRINHA")
+  (parser-b/main-terminus row-pre-boarding) => "TERM CIDADE TIRADENTES"
+  (parser-b/main-terminus row-bad-format) => "PQ RES COCAIA"
+  (parser-b/main-terminus row-normal) => "TERM CACHOEIRINHA")
 
 (fact "parser-b/auxiliar-terminus parses main terminus"
-  (parser-b/auxiliar-terminus row-type-b) => "TERM LAPA")
+		(parser-b/auxiliar-terminus row-pre-boarding) => "VIP"
+  (parser-b/auxiliar-terminus row-bad-format) => "PÇA DA SÉ"
+  (parser-b/auxiliar-terminus row-normal) => "TERM LAPA")
 
 (fact "parser-b/company parses main terminus"
-  (parser-b/company row-type-b) => "GATO PRETO")
+  (parser-b/company row-pre-boarding) => "VIP II"
+  (parser-b/company row-bad-format) => "CIDADE DUTRA"
+  (parser-b/company row-normal) => "GATO PRETO")
 
 (fact "paying pax totals are successfully parsed"
-  (parser-b/paying-cash-pax row-type-b) => 64
-  (parser-b/paying-normal-pax row-type-b) => 30
-  (parser-b/paying-work-card-pax row-type-b) => 19
-  (parser-b/paying-student-pax row-type-b) => 0
-  (parser-b/paying-rail-bus-conn-pax row-type-b) => 12
-  (parser-b/paying-month-pass-normal-pax row-type-b) => 6
-  (parser-b/paying-month-pass-work-pax row-type-b) => 0
-  (parser-b/paying-month-pass-student-pax row-type-b) => 0
-  (parser-b/paying-month-pass-rail-bus-conn-pax row-type-b) => 0
-  (parser-b/paying-pax row-type-b) => 131)
+  (parser-b/paying-cash-pax row-normal) => 64
+  (parser-b/paying-normal-pax row-normal) => 30
+  (parser-b/paying-work-card-pax row-normal) => 19
+  (parser-b/paying-student-pax row-normal) => 0
+  (parser-b/paying-rail-bus-conn-pax row-normal) => 12
+  (parser-b/paying-month-pass-normal-pax row-normal) => 6
+  (parser-b/paying-month-pass-work-pax row-normal) => 0
+  (parser-b/paying-month-pass-student-pax row-normal) => 0
+  (parser-b/paying-month-pass-rail-bus-conn-pax row-normal) => 0
+  (parser-b/paying-pax row-normal) => 131)
 
 (fact "free pax totals are successfully parsed"
-  (parser-b/free-bus-bus-conn-pax row-type-b) => 132
-  (parser-b/free-normal-pax row-type-b) => 29
-  (parser-b/free-student-pax row-type-b) => 13
-  (parser-b/free-pax row-type-b) => 174)
+  (parser-b/free-bus-bus-conn-pax row-normal) => 132
+  (parser-b/free-normal-pax row-normal) => 29
+  (parser-b/free-student-pax row-normal) => 13
+  (parser-b/free-pax row-normal) => 174)
 
 (fact "total pax is parsed"
-  (parser-b/total-pax row-type-b) => 305)
+  (parser-b/total-pax row-normal) => 305)
 
 (fact "parser-b/parse returns a map with all information"
-  (parser-b/parse row-type-b) => {:line-id "N10511"
+		(parser-b/parse row-pre-boarding) => (contains {:pre-boarding true})
+  (parser-b/parse row-exp-tiradentes) => (contains {:pre-boarding true})
+  (parser-b/parse row-bad-format) => (contains {:pre-boarding false
+                                                :main-terminus "PQ RES COCAIA"
+                                                :auxiliar-terminus "PÇA DA SÉ"
+                                                :line-id "536210"})
+  (parser-b/parse row-normal) => {:pre-boarding false
+  																																:line-id "N10511"
                                   :line-code "N105"
                                   :branch-code "11"
                                   :main-terminus "TERM CACHOEIRINHA"
