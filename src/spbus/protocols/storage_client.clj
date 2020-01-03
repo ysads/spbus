a702250b47d3bdad3cb3d046faf054d4f531e163
Ygor Sad <ysads2010@gmail.com>
2020-01-03 08:58:16 -0300

    [storage] create client protocol and find and insert functions

---
+++ src/spbus/protocols/storage_client.clj
@@ -0,0 +1,12 @@
+(ns spbus.protocols.storage-client
+  (:gen-class))
+
+(defprotocol StorageClient
+  "A protocol which defines the contract between storage adapters
+  and the application."
+  (find-by-id [storage entity id]
+    "Returns a single object based on its unique identifier.")
+  (find [storage entity conditions]
+    "Returns a collection of objects based on given conditions.")
+  (put! [storage entity data]
+    "Insert a new object to database under given entity."))
