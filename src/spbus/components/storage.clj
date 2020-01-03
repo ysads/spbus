7b8636d8f3c4e29ae75c9603afc0debf79869bc9
Ygor Sad <ysads2010@gmail.com>
2020-01-03 08:58:16 -0300

    [storage] create client protocol and find and insert functions

--- src/spbus/components/storage.clj
+++ src/spbus/components/storage.clj
@@ -1,7 +1,9 @@
 (ns spbus.components.storage
   (:require [com.stuartsierra.component :as component]
-            [monger.core :as monger])
-  (:import [com.mongodb MongoOptions ServerAddress])
+            [monger.core :as monger]
+            [monger.collection :as monger-data]
+            [spbus.protocols.storage-client :as storage-client])
+  (:import org.bson.types.ObjectId)
   (:gen-class))
 
 (defn ^:private setup-db-conn
@@ -15,12 +17,42 @@
   (monger/disconnect (:conn this))
   (update-in this [:storage] dissoc :conn :storage))
 
+(defn ^:private object-id
+  "Converts between string and ObjectId instances, always returning a valid
+  object ID for this storage client."
+  [id]
+  (if (instance? ObjectId id)
+    id
+    (ObjectId. id)))
+
+(defn ^:private find-one!
+  [db entity id]
+  (let [oid (object-id id)]
+    (monger-data/find-map-by-id db entity oid)))
+
+(defn ^:private insert-data!
+  [db entity data]
+  (-> (monger-data/insert-and-return db entity data)
+      :_id))
+
+(defn ^:private find-collection!
+  [db entity conditions]
+  (monger-data/find-maps db entity conditions))
+
 (defrecord MongoStorage [storage]
   component/Lifecycle
   (start [this]
     (setup-db-conn this))
   (stop [this]
-    (close-db-conn this)))
+    (close-db-conn this))
+
+  storage-client/StorageClient
+  (find-by-id [_this entity id]
+    (find-one! (:db storage) entity id))
+  (find [_this entity conditions]
+    (find-collection! (:db storage) entity conditions))
+  (put! [_this entity data]
+    (insert-data! (:db storage) entity data)))
 
 (defn new-storage
   [system]
