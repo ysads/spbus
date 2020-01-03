b33c58b9d244be63a2fa6536042449cc194d45f8
Ygor Sad <ygor.sad@revelo.com.br>
2020-01-03 20:22:28 -0300

    [storage_test] create basic tests to put! and find-by-id

--- test/spbus/components/storage_test.clj
+++ test/spbus/components/storage_test.clj
@@ -8,7 +8,23 @@
 (def *db* (:db (:storage *storage*)))
 (def test-entity "test")
 
-(fact "storage allows insertion using put!"
-  (client/put! *storage* test-entity {:foo "bar"})
-  *db* => 1
-  (monger-data/count *db* test-entity {:foo "bar"}) => 1)
+(defn clean-db []
+  (monger-data/remove *db* test-entity))
+
+(with-state-changes [(before :facts (clean-db))]
+  (facts "about put!"
+    (fact "it persists data to mongoDB"
+      (client/put! *storage* test-entity {:foo "bar"})
+      (monger-data/count *db* test-entity {:foo "bar"}) => 1))
+
+  (facts "about find-by-id"
+    (fact "finds records using ObjectId instances"
+      (let [oid (client/put! *storage* test-entity {:foo "bar"})]
+        (client/find-by-id *storage* test-entity oid) => {:_id oid
+                                                          :foo "bar"}))
+
+    (fact "finds records using string"
+      (let [oid (client/put! *storage* test-entity {:foo "bar"})
+            string-id (.toString oid)]
+        (client/find-by-id *storage* test-entity string-id) => {:_id oid
+                                                                :foo "bar"}))))
