374a9adc55410cbf8ae2c6cdf59a55ea455ef6fb
Ygor Sad <ysads2010@gmail.com>
2020-01-05 23:06:12 -0300

    [test] improve test coverage and add new test cases

--- test/spbus/components/storage_test.clj
+++ test/spbus/components/storage_test.clj
@@ -1,82 +1,98 @@
 (ns spbus.components.storage-test
-  (:require [failjure.core :as fail]
+  (:require [com.stuartsierra.component :as component]
+            [failjure.core :as fail]
             [java-time :as time]
             [midje.sweet :refer :all]
-            [monger.collection :as monger-data]
+            [monger.collection :as monger-document]
             [monger.operators :refer :all]
             [spbus.components.storage :as storage]
             [spbus.protocols.storage-client :as client]))
 
-(def *storage* (.start (storage/new-storage {})))
-(def *db* (:db (:storage *storage*)))
+(def *storage* nil)
+(def *db* nil)
 (def test-entity "test")
 
+(defn setup-db []
+  (alter-var-root #'*storage* (fn [_] (component/start (storage/new-storage {}))))
+  (alter-var-root #'*db* (fn [_] (:db (:storage *storage*)))))
+
+(defn close-db []
+  (alter-var-root #'*storage* #(component/stop %)))
+
 (defn clean-db []
-  (monger-data/remove *db* test-entity))
+  (monger-document/remove *db* test-entity))
 
-(with-state-changes [(before :facts (clean-db))]
+(with-state-changes [(before :contents (setup-db))
+                     (before :facts (clean-db))
+                     (after :contents (close-db))]
   (facts "about put!"
-    (fact "it persists data to mongoDB"
+    (fact "it persists document to mongoDB"
       (client/put! *storage* test-entity {:foo "bar"})
-      (monger-data/count *db* test-entity {:foo "bar"}) => 1)
+      (monger-document/count *db* test-entity {:foo "bar"}) => 1)
 
-    (fact "it assocs :created-at to data being persisted"
+    (fact "it assocs :created-at to document being persisted"
       (let [now (time/local-date-time)]
-        (client/put! *storage* test-entity {:foo "bar"}) => (contains {:created-at (str now)})
+        (client/put! *storage* test-entity {:foo "bar"})
+          => (contains {:created-at (str now)})
         (provided (time/local-date-time) => now)))
 
-    (fact "it assocs :updated-at to data being persisted"
+    (fact "it assocs :updated-at to document being persisted"
       (let [now (time/local-date-time)]
-        (client/put! *storage* test-entity {:foo "bar"}) => (contains {:updated-at (str now)})
+        (client/put! *storage* test-entity {:foo "bar"})
+          => (contains {:updated-at (str now)})
         (provided (time/local-date-time) => now))))
 
   (facts "about update!"
-    (fact "it merges update data into persisted data"
-      (let [data (client/put! *storage* test-entity {:foo 1 :bar 2})
-            oid (:_id data)]
-        (client/update! *storage* test-entity oid {:foo 0 :baz 3}) => (contains {:foo 0
-                                                                                 :bar 2
-                                                                                 :baz 3})))
+    (fact "it merges update document into persisted document"
+      (let [document (client/put! *storage* test-entity {:foo 1 :bar 2})
+            doc-id (:_id document)]
+        (client/update! *storage* test-entity doc-id {:foo 0 :baz 3})
+          => (contains {:foo 0
+                        :bar 2
+                        :baz 3})))
 
-    (fact "it touches :updated-at attribute of data being persisted"
-      (let [data (client/put! *storage* test-entity {:foo "bar"})
-            oid (:_id data)
-            updated-data (client/update! *storage* test-entity oid {:foo 0})]
-        (:created-at updated-data) => (:created-at data)
-        (:updated-at updated-data) =not=> (:updated-at data))))
+    (fact "it touches :updated-at attribute of document being persisted"
+      (let [document (client/put! *storage* test-entity {:foo "bar"})
+            doc-id (:_id document)
+            updated-document (client/update! *storage* test-entity doc-id {:foo 0})]
+        (:created-at updated-document) => (:created-at document)
+        (:updated-at updated-document) =not=> (:updated-at document))))
 
   (facts "about find-by-id"
     (fact "finds records using ObjectId instances"
-      (let [data (client/put! *storage* test-entity {:foo "bar"})
-            oid (:_id data)]
-        (client/find-by-id *storage* test-entity oid) => (contains {:_id oid
-                                                                    :foo "bar"})))
+      (let [document (client/put! *storage* test-entity {:foo "bar"})
+            doc-id (:_id document)]
+        (client/find-by-id *storage* test-entity doc-id)
+          => (contains {:_id doc-id
+                        :foo "bar"})))
 
     (fact "finds records using string"
-      (let [data (client/put! *storage* test-entity {:foo "bar"})
-            oid (:_id data)
-            string-id (.toString (:_id data))]
-        (client/find-by-id *storage* test-entity string-id) => (contains {:_id oid
-                                                                          :foo "bar"}))))
+      (let [document (client/put! *storage* test-entity {:foo "bar"})
+            doc-id (:_id document)
+            string-id (.toString (:_id document))]
+        (client/find-by-id *storage* test-entity string-id)
+          => (contains {:_id doc-id
+                        :foo "bar"}))))
 
   (facts "about find"
     (fact "finds a collection of records that match given conditions"
       (client/put! *storage* test-entity {:foo 1})
       (client/put! *storage* test-entity {:bar 2})
       (client/put! *storage* test-entity {:baz 3})
-      (client/find *storage* test-entity {$or [{:foo 1} {:bar 2}]}) => (just (contains {:foo 1})
-                                                                             (contains {:bar 2}) :in-any-order)))
+      (client/find *storage* test-entity {$or [{:foo 1} {:bar 2}]})
+        => (just (contains {:foo 1})
+                 (contains {:bar 2}) :in-any-order)))
 
   (facts "about delete!"
     (fact "it does not allow deletion without conditions"
       (let [error (client/delete! *storage* test-entity {})]
         (fail/message error)=> "Can not delete without conditions"))
 
-    (fact "removes from database records which match conditions"
+    (fact "removes from document records which match conditions"
       (client/put! *storage* test-entity {:foo 1})
       (client/put! *storage* test-entity {:bar 2})
       (client/put! *storage* test-entity {:baz 3})
       (client/delete! *storage* test-entity {$or [{:foo 1} {:bar 2}]})
-      (monger-data/count *db* test-entity {:foo 1}) => 0
-      (monger-data/count *db* test-entity {:bar 2}) => 0
-      (monger-data/count *db* test-entity {:baz 3}) => 1)))
+      (monger-document/count *db* test-entity {:foo 1}) => 0
+      (monger-document/count *db* test-entity {:bar 2}) => 0
+      (monger-document/count *db* test-entity {:baz 3}) => 1)))
