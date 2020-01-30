555f09e1dc16aff8b8f5334f5897037656d0b1a2
Ygor Sad <ygor.sad@revelo.com.br>
2020-01-30 12:05:25 -0300

    test(system-utils): add test for stop-system

--- test/spbus/system_utils_test.clj
+++ test/spbus/system_utils_test.clj
@@ -11,4 +11,11 @@
       @system-utils/current-system => nil
       (system-utils/running-system-for-env :test)
       @system-utils/current-system => (just {:config anything
-                                             :storage anything}))))
+                                             :storage anything})))
+
+  (fact "system-utils/stop-system stops the current active system"
+        @system-utils/current-system => nil
+        (system-utils/running-system-for-env :test)
+        (keys (:storage @system-utils/current-system)) => (just #{:config :conn :db}) 
+        (system-utils/stop-system)
+        (keys (:storage @system-utils/current-system)) => (just #{:config})))
